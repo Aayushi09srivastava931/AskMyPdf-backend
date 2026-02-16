5bb1b40d23264c7c5263224a6a09e4af76853295
Aayushi Srivastava <aayushisrivastava83@gmail.com>
2026-02-16 23:18:29 +0530

    new database

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ const PORT = process.env.PORT || 5001;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://localhost:5173',
-    'https://ask-my-pdf-frontend-mu.vercel.app'
+    'http://localhost:5173'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
