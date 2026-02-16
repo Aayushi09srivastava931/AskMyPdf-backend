0a712fa7ea0dc462cb8f4bd8069004c552d3c89b
Aayushi Srivastava <aayushisrivastava83@gmail.com>
2026-02-16 23:51:50 +0530

    Update CORS for production

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    "https://ask-my-pdf-frontend-eta.vercel.app"
+    "https://ask-my-pdf-frontend-tau.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
