b73ae6fb86fdc904002c584c2947f3b82f17de77
Aayushi Srivastava <aayushisrivastava83@gmail.com>
2026-02-17 01:16:13 +0530

    redeploy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    "https://ask-my-pdf-frontend-tau.vercel.app"
+    "https://ask-my-pdf-frontend-gau8ujc5s-aayushi-srivastavas-projects.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
