8a9b35c4f56d7775e7273361a3a196c1bde227c3
Aayushi Srivastava <aayushisrivastava83@gmail.com>
2026-02-16 22:51:37 +0530

    updated cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://vector-mind-frontend.vercel.app'
+    'https://ask-my-pdf-frontend-mu.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
